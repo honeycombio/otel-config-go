d23bbf7e7b4ef9fe0f43894fc6e9805239a176f7
Robb Kidd <robbkidd@honeycomb.io>
2023-06-01 17:06:40 -0400

    add smoke tests for the WithResource config func
    Per the OTel spec, resource attributes set via environment variable must
    supercede resource attributes set in code.[1]
    
    [1] https://opentelemetry.io/docs/specs/otel/resource/sdk/#specifying-resource-information-via-an-environment-variable

--- examples/main.go
+++ examples/main.go
@@ -5,12 +5,21 @@ import (
 	"log"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
 
 	"github.com/honeycombio/otel-config-go/otelconfig"
 )
 
 func main() {
-	otelShutdown, err := otelconfig.ConfigureOpenTelemetry()
+	otelShutdown, err := otelconfig.ConfigureOpenTelemetry(
+		otelconfig.WithResourceOption(
+			resource.WithAttributes(
+				attribute.String("resource.example_set_in_code", "CODE"),
+				attribute.String("resource.example_clobber", "CODE_WON"),
+			),
+		),
+	)
 
 	if err != nil {
 		log.Fatalf("error setting up OTel SDK - %e", err)
@@ -18,7 +27,6 @@ func main() {
 
 	defer otelShutdown()
 	tracer := otel.Tracer("my-app")
-	ctx := context.Background()
-	ctx, span := tracer.Start(ctx, "doing-things")
+	_, span := tracer.Start(context.Background(), "doing-things")
 	defer span.End()
 }
